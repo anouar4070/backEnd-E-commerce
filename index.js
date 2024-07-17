7970e161d1f1f898bea836b92fabdd5721eb5ca2
anouar garbaa <g.anouar@yahoo.com>
2024-07-17 16:17:42 +0100

    add AppError class

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ import express from 'express';
 import { dbConnection } from './database/dbConnection.js';
 import dotenv from "dotenv";
 import categoryRouter from './src/modules/categories/categories.routes.js';
+import morgan from 'morgan';
+import AppError from './src/utils/AppError.js';
+
 
 const app = express();
 const port = 3000;
 dotenv.config();
 
 app.use(express.json());
-
+app.use(morgan('dev'))
 app.use("/api/v1/category", categoryRouter )
 
-app.all("*", (req, res) => {
-  res.json({message: `can't find this route:${req.originalUrl}`})
+
+//Route not found:
+app.all("*", (req, res, next) => {
+  // res.json({message: `can't find this route:${req.originalUrl}`})
+  next(new AppError(`can't find this route:${req.originalUrl}`, 404))
   
+}
+)
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.status(err.statusCode).json(err.message)
 }
 )
 
@@ -21,3 +32,8 @@ dbConnection();
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+process.on('unhandledRejection', (err) => {
+  console.log(err)
+}
+)
--- src/modules/categories/categories.controller.js
+++ src/modules/categories/categories.controller.js
@@ -1,42 +1,58 @@
 import { CategoryModel } from "../../../models/category.model.js";
 import slugify from "slugify";
+import AppError from "../../utils/AppError.js";
 
-const createCategory = async (req, res, next) => {
+const catchAsyncError = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch( err=> {
+      next(err);
+    });
+  };
+};
+
+const createCategory = catchAsyncError(async (req, res, next) => {
   let { name } = req.body;
   // await CategoryModel.insertMany({name, slug: name})
   // await CategoryModel.create({name, slug: name})
-  let results = new CategoryModel({ name, slug:slugify(name) });
+  let results = new CategoryModel({ name, slug: slugify(name) });
   let added = await results.save();
-  res.status(201).json({ message: "added" , added});
-};
+  res.status(201).json({ message: "added", added });
+});
 
-const getAllCategories = async (req, res, next) => {
+const getAllCategories = catchAsyncError(async (req, res, next) => {
   let results = await CategoryModel.find({});
   res.json({ message: "Done", results });
-};
+});
 
-const getCategoryById = async (req, res, next) => {
+const getCategoryById = catchAsyncError(async (req, res, next) => {
   let { id } = req.params;
   let results = await CategoryModel.findById(id);
-  res.json({ message: "Done", results });
-};
+  if (results) return res.json({ message: "Done", results });
+  res.json({ message: "category doesn't exist" });
+});
 
-const updateCategory = async (req, res, next) => {
+const updateCategory = catchAsyncError(async (req, res, next) => {
   let { id } = req.params;
   let { name } = req.body;
   let results = await CategoryModel.findByIdAndUpdate(
     id,
-    { name, slug:slugify(name)},
+    { name, slug: slugify(name) },
     { new: true }
   );
-  res.json({ message: "Done", results });
-};
+  // res.json({ message: "Done", results });
+  
+  !results && next(new AppError("category not found", 404))
+  results && res.json({ message: "Deleted" });
 
-const deleteCategory = async (req, res, next) => {
+});
+
+const deleteCategory = catchAsyncError(async (req, res, next) => {
   let { id } = req.params;
   let results = await CategoryModel.findByIdAndDelete(id);
-  res.json({ message: "Done", results });
-};
+  !results && res.status(404).json({ message: "category not found" });
+
+  results && res.json({ message: "Deleted" });
+});
 
 export {
   getAllCategories,
