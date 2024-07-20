ba71ccfd647646e79ed16900845cb278cba33cad
anouar garbaa <g.anouar@yahoo.com>
2024-07-20 15:50:52 +0100

    add comments

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api/v1/subcategory", subCategoryRouter )
 app.use("/api/v1/brand", brandRouter )
 
 
-//& Route not found:
+//* Route not found:
 app.all("*", (req, res, next) => {
   // res.json({message: `can't find this route:${req.originalUrl}`})
   next(new AppError(`can't find this route:${req.originalUrl}`, 404))
@@ -27,20 +27,20 @@ app.all("*", (req, res, next) => {
 }
 )
 
-//& global error handling
+//* global error handling
 app.use((err, req, res, next) => {
   console.log(err)
   res.status(err.statusCode).json(err.message)
 }
 )
-
+//* Initialize the database connection
 dbConnection();
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-//& ensure that unprocessed promise rejections are captured
+//* ensure that unprocessed promise rejections are captured
 process.on('unhandledRejection', (err) => {
   console.log(err)
 }
