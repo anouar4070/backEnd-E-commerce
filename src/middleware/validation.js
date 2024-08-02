82d86a418db069dae577a2bcda870d19081bcaac
anouar garbaa <g.anouar@yahoo.com>
2024-08-02 19:39:39 +0100

    add back-end validation

---
+++ src/middleware/validation.js
@@ -0,0 +1,22 @@
+export const validation = (schema) => {
+  return (req, res, next) => {
+
+    let inputs = {...req.body, ...req.params, ...req.query}
+    let { error } = schema.validate(inputs, { abortEarly: false });
+    if (error) {
+
+      let errors = error.details.map((detail) => detail.message);
+      res.json(errors);
+    } else {
+      next()
+    }
+  };
+};
+
+
+
+/**
+ * req.body {name: "anouar"}
+ * req.params {id: 21321231}
+ * req.query 
+ */
